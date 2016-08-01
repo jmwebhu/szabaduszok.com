bdf5314bcd96a2bb7c8bee5db3649cb741ae27e2
Joo Martin <joomartin@jmweb.hu>
2016-08-01 23:27:21 +0200

    Projekt ertesito limit
    50 helyett 25, szerver SMTP hiba miatt

--- application/classes/Controller/Test.php
+++ application/classes/Controller/Test.php
@@ -4,24 +4,9 @@ class Controller_Test extends Controller
 {
     public function action_index()
     {	        
-		$data = [
-			//['name' => 'Joó Martin', 'number' => '9', 'float' => 1.34, 'float1' => 1.543],
-			['name' => 'Bognár Mariann', 'number' => 12, 'float' => 12.4, 'float1' => 2.742],
-			//['name' => 'Lionel Messi', 'number' => 8, 'float' => 0.9, 'float1' => 10.846],
-			//['name' => 'C. Ronaldo', 'number' => 7, 'float' => 31.947, 'float1' => 40.453]
-		];
-
-		$result = AB::select()->from($data)
-			->where('number', '>', 7)
-			->and_where_open()
-				->or_where('name', 'LIKE', 'MESSI')
-				->or_where('name', 'LIKE', 'ronaldo')
-			->and_where_close()
-			->execute()->as_array();
-		
-		// Helyes visszatérés: Lionel Messi
-		
-		echo Debug::vars('result: ', $result);
+		$pass = Model_User::generatePassword();
+		echo Debug::vars($pass);
+		echo Debug::vars(Auth::instance()->hash($pass));
     }	
 
     public function action_clearcache()
--- modules/cron/config/cron.php
+++ modules/cron/config/cron.php
@@ -2,5 +2,5 @@
 
 return [
 	// Projekt ertesito CRON egy hivasban csak ennyi usernek kuld e-mail
-	'notificationLimit'		=> 50,	
+	'notificationLimit'		=> 25,	
 ];
