f38f0ee43cba57a419dc58f27d180791499c71c2
jmwebhu <joomartin@jmweb.hu>
2016-09-29 09:49:29 +0200

    Business unittest

--- application/classes/Business.php
+++ application/classes/Business.php
@@ -7,30 +7,65 @@ class Business
      */
     private $_model;
 
-    public static function getIdsFromModelsMulti(array $models, $primaryKey = null)
+    public static function getIdsFromModelsSingle(array $models, $primaryKey = null)
     {
         $ids = [];
-        foreach ($models as $i => $item) {
-            if (!isset($ids[$i])) {
-                $ids[$i] = [];
-            }
+        foreach ($models as $model) {
 
-            foreach ($item as $model) {
-                $ids[$i][] = self::checkAndGetIdFromModel($model, $primaryKey);
+            try {
+                self::checkModel($model);
+                $ids[] = self::getIdFromModel($model, $primaryKey);
+
+            } catch (Exception $ex) {
+                Log::instance()->add(Log::ERROR, $ex->getMessage() . ' Trace: ' . $ex->getTraceAsString());
+
+                return [];
             }
         }
 
         return $ids;
     }
 
-    public static function checkAndGetIdFromModel($model, $primaryKey = null)
+    public static function getIdsFromModelsMulti(array $models, $primaryKey = null)
     {
-        if ($model instanceof ORM) {
-            if ($primaryKey) {
-                return $model->{$primaryKey};
+        $ids = [];
+        foreach ($models as $i => $array) {
+            if (!is_array($array)) {
+                return [];
             }
 
-            return $model->pk();
+            $ids[$i] = self::getIdsFromModelsSingle($array, $primaryKey);
         }
+
+        return $ids;
+    }
+
+    protected static function checkModel($model)
+    {
+        if (!$model instanceof ORM) {
+            throw new Exception('Instead of ORM, ' . get_class($model) . ' was given');
+        }
+
+        return true;
+    }
+
+    protected static function getIdFromModel(ORM $model, $primaryKey = null)
+    {
+        if ($primaryKey && self::checkPrimaryKey($model, $primaryKey)) {
+            return $model->{$primaryKey};
+        }
+
+        return $model->pk();
+    }
+
+    protected static function checkPrimaryKey(ORM $model, $primaryKey)
+    {
+        $object = $model->object();
+
+        if (!Arr::get($object, $primaryKey)) {
+            throw new Exception('Trying to get non-existing property ' . $primaryKey . ' in class ' . get_class($model));
+        }
+
+        return true;
     }
 }
