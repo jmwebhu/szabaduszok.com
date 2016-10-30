cf078d0a985302f53b5a7b7006f8878c8d75e47f
jmwebhu <joomartin@jmweb.hu>
2016-10-30 11:35:50 +0100

    Notifier konstruktor bugfix
    Nem lehet parametere az Entity_User konstruktorban levo init miatt

--- modules/notification/tests/Notification/Formatter/Test.php
+++ modules/notification/tests/Notification/Formatter/Test.php
@@ -7,7 +7,8 @@ class Notification_Formatter_Test extends Unittest_TestCase
         $event = Model_Event_Factory::createEvent(Model_Event::TYPE_PROJECT_NEW);
         $this->setMockAny('\Notification', 'getEvent', $event);
 
-        $notifier = new Notifier_Email($this->_mock);
+        $notifier = new Notifier_Email();
+        $notifier->setNotification($this->_mock);
         $formatter = new Notification_Formatter($this->_mock, $notifier);
         $template = $this->invokeMethod($formatter, 'getFullTemplatePath', []);
 
