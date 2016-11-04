d590bd3f27b8c54cf773b298f9f6a452df1e69f7
jmwebhu <joomartin@jmweb.hu>
2016-11-04 13:58:46 +0100

    Jelentkezes
    JS

--- modules/project/media/js/sites/project-partner.js
+++ modules/project/media/js/sites/project-partner.js
@@ -7,16 +7,20 @@ var ProjectPartner = {
         this.$apply             = $('a#apply');
         this.$undoApplication   = $('a#undoApplication');
 
-        this.$useShortDescription = $('a#use-short-description');
+        this.$useShortDescription = $('div.project-partner-fancybox a#use-short-description');
+        this.$cancel = $('div.project-partner-fancybox a.cancel');
+        this.$operation = $('div.project-partner-fancybox a.operation');
     },
     bindEvents: function () {
         this.$apply.click(ProjectPartner.applyClick);
         this.$undoApplication.click(ProjectPartner.undoApplicationClick);
 
         this.$useShortDescription.click(ProjectPartner.useShortDescriptionClick);
+        this.$cancel.click(ProjectPartner.cancelClick);
+        this.$operation.click(ProjectPartner.operationClick);
     },
     applyClick: function () {
-        ProjectPartner.openFancybox('Jelentkezés');
+        ProjectPartner.openFancybox();
         return false;
         /*var $this = $(this);
         $this.prop('disabled', true);
@@ -44,15 +48,28 @@ var ProjectPartner = {
     undoApplicationClick: function () {
 
     },
-    openFancybox: function (title) {
+    openFancybox: function () {
         var myOption =  $.extend(true, {}, fancyBoxOptions);
         myOption.href = '#project-partner-fancybox-candidate';
         myOption.height = 500;
+
+        myOption.afterLoad = function () {
+            $('.project-partner-fancybox textarea').val('');
+        };
 
         $.fancybox.open(myOption);
     },
     useShortDescriptionClick: function () {
         $('textarea#message').val(USER.short_description);
+        return false;
+    },
+    cancelClick: function () {
+        $.fancybox.close();
+        return false;
+    },
+    operationClick: function () {
+        console.log('ope');
+
         return false;
     }
 };
