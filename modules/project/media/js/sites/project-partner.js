c0ea24db63a774f70ff8104b203c68095a00b1c0
jmwebhu <joomartin@jmweb.hu>
2016-11-06 16:28:55 +0100

    jshint + bugfix

--- modules/project/media/js/sites/project-partner.js
+++ modules/project/media/js/sites/project-partner.js
@@ -4,15 +4,15 @@ var ProjectPartner = {
         this.bindEvents();
     },
     cacheElements: function () {
-        this.$apply                 = $('a#apply');
-        this.$undoApplication       = $('a#undoApplication');
-        this.$approveApplication    = $('a.approveApplication');
-        this.$rejectApplication     = $('a.rejectApplication');
-        this.$cancelParticipation   = $('a.cancelParticipation');
+        this.$apply                 = $("a#apply");
+        this.$undoApplication       = $("a#undoApplication");
+        this.$approveApplication    = $("a.approveApplication");
+        this.$rejectApplication     = $("a.rejectApplication");
+        this.$cancelParticipation   = $("a.cancelParticipation");
 
-        this.$useShortDescription   = $('div.project-partner-fancybox a#use-short-description');
-        this.$cancel                = $('div.project-partner-fancybox button.cancel');
-        this.$operation             = $('div.project-partner-fancybox button.operation');
+        this.$useShortDescription   = $("div.project-partner-fancybox a#use-short-description");
+        this.$cancel                = $("div.project-partner-fancybox button.cancel");
+        this.$operation             = $("div.project-partner-fancybox button.operation");
 
     },
     bindEvents: function () {
@@ -27,42 +27,42 @@ var ProjectPartner = {
         this.$operation.click(ProjectPartner.operationClick);
     },
     applyClick: function () {
-        ProjectPartner.openFancybox('candidate');
+        ProjectPartner.openFancybox("candidate");
         return false;
     },
     undoApplicationClick: function () {
-        ProjectPartner.openFancybox('undo-application');
+        ProjectPartner.openFancybox("undo-application");
         return false;
     },
     approveApplicationClick: function () {
-        $('input[name="project_partner_id"]').val($(this).data('project_partner_id'));
-        ProjectPartner.openFancybox('approve-application');
+        $("input[name='project_partner_id']").val($(this).data("project_partner_id"));
+        ProjectPartner.openFancybox("approve-application");
         return false;
     },
     rejectApplicationClick: function () {
-        $('input[name="project_partner_id"]').val($(this).data('project_partner_id'));
-        ProjectPartner.openFancybox('reject-application');
+        $("input[name='project_partner_id']").val($(this).data("project_partner_id"));
+        ProjectPartner.openFancybox("reject-application");
         return false;
     },
     cancelParticipationClick: function () {
-        $('input[name="project_partner_id"]').val($(this).data('project_partner_id'));
-        ProjectPartner.openFancybox('cancel-participation');
+        $("input[name='project_partner_id']").val($(this).data("project_partner_id"));
+        ProjectPartner.openFancybox("cancel-participation");
         return false;
     },
     openFancybox: function (target) {
         var myOption =  $.extend(true, {}, fancyBoxOptions);
-        myOption.href = '#project-partner-fancybox-' + target;
+        myOption.href = "#project-partner-fancybox-" + target;
         myOption.height = 500;
 
         myOption.afterLoad = function () {
-            $('.project-partner-fancybox textarea').val('');
-            $('.project-partner-fancybox button').prop('disabled', false);
+            $(".project-partner-fancybox textarea").val("");
+            $(".project-partner-fancybox button").prop("disabled", false);
         };
 
         $.fancybox.open(myOption);
     },
     useShortDescriptionClick: function () {
-        $('textarea#message').val(USER.short_description);
+        $("textarea#message").val(USER.short_description);
         return false;
     },
     cancelClick: function () {
@@ -72,7 +72,7 @@ var ProjectPartner = {
     operationClick: function () {
         var $this = $(this);
 
-        var operation = OperationFactory.create($this.data('operation'));
+        var operation = OperationFactory.create($this.data("operation"));
         operation.doOperation($this);
 
         return false;
@@ -85,71 +85,71 @@ var OperationBase = {
     getFormName: function () {},
     doOperation: function ($button) {
         var self = this;
-        $button.prop('disabled', true);
+        $button.prop("disabled", true);
         var ajax = new AjaxBuilder();
 
         var beforeSend = function () {
-            Default.startLoading($('div.project-partner-fancybox span.loading'));
+            Default.startLoading($("div.project-partner-fancybox span.loading"));
         };
 
         var success = function (data) {
-            Default.stopLoading(data.error, 'Sikeres ' + self.getOperationLang(), $('div.project-partner-fancybox span.loading'));
+            Default.stopLoading(data.error, "Sikeres " + self.getOperationLang(), $("div.project-partner-fancybox span.loading"));
         };
 
         var error = function () {
-            Default.stopLoading(true, 'Sikeres ' + self.getOperationLang(), $('div.project-partner-fancybox span.loading'));
+            Default.stopLoading(true, "Sikeres " + self.getOperationLang(), $("div.project-partner-fancybox span.loading"));
         };
 
         var complete = function(data) {
             location.reload();
         };
 
-        ajax.url(ROOT + 'projectpartner/ajax/' + self.getAjaxTarget()).data($('form#' + self.getFormName() + '-form').serialize())
+        ajax.url(ROOT + "projectpartner/ajax/" + self.getAjaxTarget()).data($("form#" + self.getFormName() + "-form").serialize())
             .beforeSend(beforeSend).success(success).complete(complete).error(error).send();
     }
 };
 
 var Apply = $.extend(true, {}, OperationBase);
-Apply.getOperationLang = function () { return 'jelentkezés'; };
-Apply.getAjaxTarget = function() { return 'apply'; };
-Apply.getFormName = function () { return 'apply'; };
+Apply.getOperationLang = function () { return "jelentkezés"; };
+Apply.getAjaxTarget = function() { return "apply"; };
+Apply.getFormName = function () { return "apply"; };
 
 var UndoApplication = $.extend(true, {}, OperationBase);
-UndoApplication.getOperationLang = function () { return 'visszavonás'; };
-UndoApplication.getAjaxTarget = function() { return 'undoApplication'; };
-UndoApplication.getFormName = function () { return 'undo-application'; };
+UndoApplication.getOperationLang = function () { return "visszavonás"; };
+UndoApplication.getAjaxTarget = function() { return "undoApplication"; };
+UndoApplication.getFormName = function () { return "undo-application"; };
 
 var ApproveApplication = $.extend(true, {}, OperationBase);
-ApproveApplication.getOperationLang = function () { return 'jóváhagyás'; };
-ApproveApplication.getAjaxTarget = function() { return 'approveApplication'; };
-ApproveApplication.getFormName = function () { return 'approve-application'; };
+ApproveApplication.getOperationLang = function () { return "jóváhagyás"; };
+ApproveApplication.getAjaxTarget = function() { return "approveApplication"; };
+ApproveApplication.getFormName = function () { return "approve-application"; };
 
 var RejectApplication = $.extend(true, {}, OperationBase);
-RejectApplication.getOperationLang = function () { return 'elutasítás'; };
-RejectApplication.getAjaxTarget = function() { return 'rejectApplication'; };
-RejectApplication.getFormName = function () { return 'reject-application'; };
+RejectApplication.getOperationLang = function () { return "elutasítás"; };
+RejectApplication.getAjaxTarget = function() { return "rejectApplication"; };
+RejectApplication.getFormName = function () { return "reject-application"; };
 
 var CancelApplication = $.extend(true, {}, OperationBase);
-CancelApplication.getOperationLang = function () { return 'törlés'; };
-CancelApplication.getAjaxTarget = function() { return 'cancelParticipation'; };
-CancelApplication.getFormName = function () { return 'cancel-participation'; };
+CancelApplication.getOperationLang = function () { return "törlés"; };
+CancelApplication.getAjaxTarget = function() { return "cancelParticipation"; };
+CancelApplication.getFormName = function () { return "cancel-participation"; };
 
 var OperationFactory = {
     create: function (operation) {
         switch (operation) {
-            case 'apply':
+            case "apply":
                 return Apply;
 
-            case 'undo-application':
+            case "undo-application":
                 return UndoApplication;
 
-            case 'approve-application':
+            case "approve-application":
                 return ApproveApplication;
 
-            case 'reject-application':
+            case "reject-application":
                 return RejectApplication;
 
-            case 'cancel-participation':
+            case "cancel-participation":
                 return CancelApplication;
         }
     }
