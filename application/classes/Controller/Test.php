7162bace126e047249064b2152dafe341ea8a2c9
jmwebhu <joomartin@jmweb.hu>
2016-11-11 18:09:43 +0100

    PayPal API teszt

--- application/classes/Controller/Test.php
+++ application/classes/Controller/Test.php
@@ -4,16 +4,42 @@ class Controller_Test extends Controller
 {
     public function action_index()
     {
-        $res = DB::select()
-            ->from('projects_partners')
-            ->where('user_id', '=', 2022)
-            ->and_where('project_id', '=', 10023)
-            ->and_where('type', '=', Model_Project_Partner::TYPE_CANDIDATE)
-            ->limit(1)
-            ->execute()->count();
+        $header = "Content-type: application/json\r\n";
+        $header .= "Authorization: Bearer AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS\r\n";
 
+        $data = [
+            "intent" => "sale",
+            "payer" => [
+                "payment_method"    => "paypal"
+            ],
+            "payee" => [
+                "email" => "szamlazas@jmweb.hu"
+            ],
+            "transactions" => [
+                "amount" => [
+                    "total" => "45000",
+                    "currency"  => "HUF"
+                ]
+            ],
+            "description" => "Projekt neve",
+            "redirect_urls" => [
+                "return_url" => "http://127.0.0.1/szabaduszok.com/success=1",
+                "cancel_url" => "http://127.0.0.1/szabaduszok.com/success=0"
+            ]
+        ];
 
-        echo Debug::vars($res);
+        $options = [
+            'http' => [
+                'header'  => $header,
+                'method'  => 'POST',
+                'content' => json_encode($data)
+            ]
+        ];
+        
+        $context = stream_context_create($options);
+        $response = file_get_contents("https://api.paypal.com/v1/payments/payment", false, $context);
+
+        echo Debug::vars($response);
     }
 
     public function action_clearcache()
