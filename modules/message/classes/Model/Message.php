6be700b597229d20fdb08fd8bda1194416475001
jmwebhu <joomartin@Joo-MacBook-Pro.local>
2016-11-29 12:47:45 +0100

    Message Model

--- modules/message/classes/Model/Message.php
+++ modules/message/classes/Model/Message.php
@@ -1,6 +1,6 @@
 <?php
 
-class Model_Message extends ORM
+class Model_Message extends ORM implements Message
 {
     protected $_table_name  = 'messages';
     protected $_primary_key = 'message_id';
@@ -10,11 +10,18 @@ class Model_Message extends ORM
         'format' => 'Y-m-d H:i'
     ];
 
+    protected $_updated_column = [
+        'column' => 'updated_at',
+        'format' => 'Y-m-d H:i'
+    ];
+
     protected $_table_columns = [
-        'message_id'    => ['type' => 'int',        'key' => 'PRI'],
-        'sender_id'     => ['type' => 'int',        'null' => true],
-        'message'       => ['type' => 'string',     'null' => true],
-        'created_at'    => ['type' => 'datetime',   'null' => true]
+        'message_id'        => ['type' => 'int',        'key' => 'PRI'],
+        'conversation_id'   => ['type' => 'int',        'null' => true],
+        'sender_id'         => ['type' => 'int',        'null' => true],
+        'message'           => ['type' => 'string',     'null' => true],
+        'created_at'        => ['type' => 'datetime',   'null' => true],
+        'updated_at'        => ['type' => 'datetime',   'null' => true]
     ];
 
     protected $_belongs_to  = [
@@ -22,10 +29,10 @@ class Model_Message extends ORM
             'model'         => 'User',
             'foreign_key'   => 'sender_id'
         ],
-        'receiver'          => [
+        'conversation'          => [
             'model'         => 'User',
-            'foreign_key'   => 'receiver_id'
-        ]
+            'foreign_key'   => 'sender_id'
+        ],
     ];
 
     public function rules()
@@ -35,5 +42,58 @@ class Model_Message extends ORM
             'receiver_id'   => [['not_empty']],
             'message'       => [['not_empty']]
         ];
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->message_id;
+    }
+
+    /**
+     * @return Entity_User
+     */
+    public function getSender()
+    {
+        return Entity_User::createUser($this->sender->type, $this->sender);
+    }
+
+    /**
+     * @return Entity_Conversation
+     */
+    public function getConversation()
+    {
+        return new Entity_Conversation($this->conversation);
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function send()
+    {
+        // TODO: Implement send() method.
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt()
+    {
+        return new DateTime($this->created_at);
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return new DateTime($this->updated_at);
     }
 }
