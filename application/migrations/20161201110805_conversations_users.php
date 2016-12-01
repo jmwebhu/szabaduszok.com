2fa30b0486e68f42114c1df21c7bf675df6b0262
jmwebhu <joomartin@Joo-MacBook-Pro.local>
2016-12-01 11:32:04 +0100

    Migration javitas

---
+++ application/migrations/20161201110805_conversations_users.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class conversations_users extends Migration
+{
+  public function up()
+  {
+     $this->create_table
+     (
+       'conversations_users',
+       array
+       (
+           'conversation_user_id'   => ['integer', 'unsigned' => true],
+           'conversation_id'        => ['integer', 'unsigned' => true],
+           'user_id'                => ['integer', 'unsigned' => true]
+       ),
+         'conversation_user_id'
+     );
+
+      $this->sql("ALTER TABLE `conversations_users` ADD FOREIGN KEY (`conversation_id`) REFERENCES `conversations`(`conversation_id`) ON DELETE SET NULL ON UPDATE CASCADE");
+      $this->sql("ALTER TABLE `conversations_users` ADD FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE");
+  }
+
+  public function down()
+  {
+      $this->sql("ALTER TABLE `conversations_users` DROP FOREIGN KEY user_id");
+      $this->sql("ALTER TABLE `conversations_users` DROP FOREIGN KEY conversation_id");
+
+     $this->drop_table('conversations_users');
+  }
+}
