304ee196bfc350eebc38bdfeaec8ac6bf5ea3965
jmwebhu <joomartin@Joo-iMac.local>
2016-12-11 15:46:39 +0100

    Kapcsolatfelvetel click

--- modules/message/classes/Controller/Message.php
+++ modules/message/classes/Controller/Message.php
@@ -4,11 +4,16 @@ class Controller_Message extends Controller
 {
     public function action_contact()
     {
-        $this->auto_render = false;
-        $slug = $this->request->param('userslug');
-        $user = new Entity_User();
-        $user = $user->getBySlug($slug);
+        $this->auto_render  = false;
 
+        $slug               = $this->request->param('userslug');
+        $user               = new Model_User();
+        $user               = $user->getBySlug($slug);
+
+        $conversation = Entity_Conversation::getConversationBetween([$user->user_id, Auth::instance()->get_user()->user_id]);
+
+        header('Location: ' . Route::url('messagesList', ['slug' => $conversation->getSlug()]), true, 302);
+        die();
     }
     
 }
