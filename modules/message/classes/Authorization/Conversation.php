bb25c2e5750f3996fff4385a2116c908432a356c
jmwebhu <joomartin@Joo-MacBook-Pro.local>
2016-12-19 14:09:46 +0100

    Authorization
    - Authorization osztalyok
    - unittesztek

---
+++ modules/message/classes/Authorization/Conversation.php
@@ -0,0 +1,29 @@
+<?php
+
+class Authorization_Conversation extends Authorization
+{
+    /**
+     * @return bool
+     */
+    public function canView()
+    {
+        return $this->isIn();
+    }
+    
+    /**
+     * @return bool
+     */
+    public function canSendMessage()
+    {
+        return $this->isIn();   
+    }
+    
+    /**
+     * @return bool
+     */
+    protected function isIn()
+    {
+        return $this->_model->has('users', $this->_user->user_id);
+    }
+    
+}
