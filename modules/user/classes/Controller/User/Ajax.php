c50d1bf2118330bbe95c432ba070f1ddb3d6aed2
Joo Martin <joomartin@jmweb.hu>
2017-01-25 08:35:32 +0100

    issue #24
    Javitva. AB::select() csak siman Model_User -t adott vissza.

--- modules/user/classes/Controller/User/Ajax.php
+++ modules/user/classes/Controller/User/Ajax.php
@@ -20,7 +20,9 @@ class Controller_User_Ajax extends Controller_Ajax
 
     protected function rate()
     {
-        $user = AB::select()->from(new Model_User())->where('user_id', '=', Input::post('user_id'))->limit(1)->execute()->current();
+        $temp = AB::select()->from(new Model_User())->where('user_id', '=', Input::post('user_id'))->limit(1)->execute()->current();
+        $user = Model_User::createUser($temp->type, $temp->user_id);
+
         $this->_jsonResponse = json_encode($user->rate(Input::post('rating')));
     }
 
