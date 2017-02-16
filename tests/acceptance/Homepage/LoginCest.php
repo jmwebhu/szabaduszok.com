e0ac96fb19781e2b2264a4fb6cd2c6a0e40f12d2
Joo Martin <joomartin@jmweb.hu>
2017-02-16 21:04:05 +0100

    Codeception Acceptance tesztek
    - Belepes
    - Elfelejtett jelszo

--- tests/acceptance/Homepage/LoginCest.php
+++ tests/acceptance/Homepage/LoginCest.php
@@ -3,9 +3,9 @@
 
 class LoginCest
 {
-    public function testLoginWithValidData(\AcceptanceTester $I)
+    public function testLoginWithValidDataFreelancer(\AcceptanceTester $I)
     {
-        $I->wantTo('belepes helyes adatokkal');
+        $I->wantTo('belepes helyes adatokkal szabaduszokent');
         $I->amOnPage('/szabaduszok-belepes');
         $I->fillField('email', 'm4rt1n.j00@gmail.com');
         $I->fillField('password', 'Deth4Life01');
@@ -13,6 +13,18 @@ class LoginCest
         $I->seeInCurrentUrl('szabaduszo/joo-martin');
         $I->see('Joó Martin');
         $I->see('Szabadúszó profil');
+    }
+
+    public function testLoginWithValidDataEmployeer(\AcceptanceTester $I)
+    {
+        $I->wantTo('belepes helyes adatokkal megbizokent');
+        $I->amOnPage('/szabaduszok-belepes');
+        $I->fillField('email', 'joomartin@jmweb.hu');
+        $I->fillField('password', 'Deth4Life01');
+        $I->click('.btn');
+        $I->seeInCurrentUrl('megbizo/joo-martin');
+        $I->see('Joó Martin');
+        $I->see('Megbízó profil');
     }
 
     public function testLoginWithInvalidEmail(\AcceptanceTester $I)
@@ -46,5 +58,44 @@ class LoginCest
         $I->click('.btn');
         $I->seeInCurrentUrl('szabaduszok-belepes');
         $I->see('Hibás e-mail vagy jelszó. Kérjük próbáld meg újra!', '.error-label');
+    }
+    
+    public function testNavigateToPasswordReminderPage(\AcceptanceTester $I)
+    {
+        $I->wantTo('navigalas elfelejtett jelszo oldalra belepes oldalrol');
+        $I->amOnPage('/szabaduszok-belepes');
+        $I->click('Elfelejtettem a jelszavam');
+        $I->seeInCurrentUrl('szabaduszok-elfelejtett-jelszo');
+        $I->see('Elfelejtett jelszó');
+        $I->see('Új jelszót kérek');
+    }
+
+    public function testNavigateToInterestedInPage(\AcceptanceTester $I)
+    {
+        $I->wantTo('navigalas erdekel oldalra belepes oldalrol');
+        $I->amOnPage('/szabaduszok-belepes');
+        $I->click('Kattints ide!');
+        $I->seeInCurrentUrl('erdekel');
+        $I->see('Csatlakozz a Szabaduszok.com közösségéhez!');
+        $I->see('Szabadúszó vagyok, csatlakozom!');
+        $I->see('Megbízó vagyok, csatlakozom!');
+    }
+    
+    public function testPasswordReminderWithInvalidEmail(\AcceptanceTester $I)
+    {
+        $I->wantTo('elfelejtett jelszo helytelen e-mail cimmel');
+        $I->amOnPage('/szabaduszok-elfelejtett-jelszo');
+        $I->click('.btn');
+        $I->see('Sajnáljuk, nincs ilyen e-mail cím. Kérjük próbáld meg egy másikkal.', '.error-label');
+    }
+
+    public function testPasswordReminderWithValidEmail(\AcceptanceTester $I)
+    {
+        $I->wantTo('elfelejtett jelszo helyes e-mail cimmel');
+        $I->amOnPage('/szabaduszok-elfelejtett-jelszo');
+        $I->fillField('email', 'm4rt1n.j00@gmail.com');
+        $I->click('.btn');
+        $I->seeInCurrentUrl('/szabaduszok-belepes');
+        $I->see('Szabaduszok.com Belépés');
     }
 }
